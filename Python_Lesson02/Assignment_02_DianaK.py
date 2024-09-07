dba0f21549e8701de27bed9b2bffa4da8d29e1d4
Dianak14 <144110362+Dianak14@users.noreply.github.com>
2024-09-07 12:34:08 +1000

    Updated Assign 2

--- Python_Lesson02/Assignment_02_DianaK.py
+++ Python_Lesson02/Assignment_02_DianaK.py
@@ -1,3 +1,5 @@
+#Assignment 2
+
 original_text = "Where is my cat?"
 shouting_text = original_text.upper()
 #print(shouting_text)
@@ -7,3 +9,4 @@ annoying_text = 10 * shouting_text
 
 print(f"My annoying text is {annoying_text}")
 print(f"It has {len(annoying_text)} characters")
+
